79b6be96eb98de1aafc9c34b30138deeb736c517
Rahul Tangsali <rahuul2001@gmail.com>
2024-11-01 16:04:52 -0700

    implement complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,48 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {
+    firstName:"Rahul", 
+    lastName:"Tangsali",
+    email:"tangsali@usc.edu"
+  }
+
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subText="Access and manage your account and transactions efficiently."
+          />
+
+          {/* balance box to display the total amount */}
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+
+        </header>
+
+        {/* component for recent transactions on right hand side */}
+        RECENT TRANSACTIONS
+        
+
+      </div>
+
+      <RightSideBar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.50}, { currentBalance:500 }]}
+      />
+    </section>
   )
 }
 
---
+++ components/Sidebar.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
+
+const Sidebar = ({ user }: SiderbarProps) => {
+
+  const pathName = usePathname();
+
+  return (
+    <div>
+        <section className="sidebar">
+            <nav className="flex flex-col gap-4">
+                <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
+                    <Image 
+                          src="/icons/logo.svg"
+                          width={34}
+                          height={34} 
+                          alt='Horizon Logo'   
+                          className='size-[24px] max-xl:size-14'                     
+                    />
+                    <h1 className="sidebar-logo">Horizon</h1>
+                </Link>
+
+                {
+                    sidebarLinks.map((item)=>{
+
+                        const isActive = pathName === item.route || pathName.startsWith(`${item.route}/`)
+
+                        return (
+                            <Link 
+                                href={item.route} 
+                                key={item.label}
+                                className={cn('sidebar-link', {
+                                    'bg-bankGradient':isActive
+                                })}
+                            >
+                                <div className='relative size-6'>
+                                    <Image
+                                        src={item.imgURL} 
+                                        alt={item.label}  
+                                        fill   
+                                        className={cn({
+                                            'brightness-[3] invert-0':isActive
+                                        })}                               
+                                    />
+                                </div>
+                                <p className={cn('sidebar-label', {
+                                    '!text-white':isActive
+                                })}>
+                                    {item.label}
+                                </p>
+                            </Link>
+                        )
+                    })
+                }
+
+            {/* will add user information here */}
+            USER
+
+            </nav>
+
+            {/* will add footer here */}
+            FOOTER
+
+        </section>
+    </div>
+  )
+}
+
+export default Sidebar
